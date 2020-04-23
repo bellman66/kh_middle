317e8543c20c383822de13447a7dce327ccfbbbd
rndso15 <48646714+rndso15@users.noreply.github.com>
2020-04-23 21:08:49 +0900

    steller Template

--- src/main/java/com/kh/middle/controller/StellerController.java
+++ src/main/java/com/kh/middle/controller/StellerController.java
@@ -1,5 +1,6 @@
 package com.kh.middle.controller;
 
+import java.sql.SQLException; 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -11,7 +12,9 @@ import org.json.simple.JSONObject;
 import org.slf4j.Logger; 
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,6 +27,7 @@ import com.kh.middle.api.op_Api.Controller.Api_Controller;
 
 @Controller
 @RequestMapping("/steller")
+@Transactional(rollbackFor = {Exception.class})
 public class StellerController {
 
 	private static final Logger logger = LoggerFactory.getLogger(StellerController.class);
@@ -86,6 +90,20 @@ public class StellerController {
 		}
 		
 		return jsonstring;
+	}
+	
+	@ExceptionHandler(value=Exception.class)
+	public ModelAndView handleException(Exception e) {
+		
+//		String getMessage() : 발생된 예외의 메시지를 리턴한다. 
+//		String toString() : 발생된 예외 클래스명과 메시지를 리턴한다. 
+//		String pritnStackTrace() : 발생된 예외를 역추적하기 위해 표준 예외 스트림을 출력한다. 
+//		예외 발생시 예외가 발생한 곳을 알아낼 때 주로 사용된다. 
+
+			
+		
+		return null;
+		
 	}
 	
 }
